307bb7d2f1d9f83d4f3566ab296f28f6da1a93b9
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-05-11 16:11:59 -0500

    Paginación Arreglo paginación del lado del frontend (para no hacer solicitudes cuando carga la página)

--- SportClub.service/src/main/webapp/src/shared/crud/crud.dir.js
+++ SportClub.service/src/main/webapp/src/shared/crud/crud.dir.js
@@ -6,21 +6,12 @@
                 scope: {
                     records: '=*',
                     model: '=',
-                    totalItems: '=',
-                    currentPage: '=',
-                    maxSize: '=',
-                    itemsPerPage: '=',
                     changePage: '&',
                     editRecord: '&',
                     deleteRecord: '&'
                 },
                 restrict: 'E',
-                templateUrl: 'src/shared/crud/list.tpl.html',
-                link: function (scope, el, attr) {
-                    scope.$watch(attr.currentPage, function () {
-                        scope.changePage();
-                    });
-                }
+                templateUrl: 'src/shared/crud/list.tpl.html'
             };
         }]);
     
