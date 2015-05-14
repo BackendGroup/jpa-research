1328e4b79caab90ace082124d69545e977db7747
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-05-14 16:14:32 -0500

    Mocks genéricos Se hacen mocks genéricos para todos los casos de CRUD básico

--- SportClub.service/src/main/webapp/src/modules/country/country.mod.js
+++ SportClub.service/src/main/webapp/src/modules/country/country.mod.js
@@ -1,13 +1,9 @@
 (function (ng) {
-    var mod = ng.module('countryModule', ['CrudModule', 'MockModule']);
+    var mod = ng.module('countryModule', ['CrudModule']);
 
     mod.constant('country.context', 'countries');
 
-    mod.constant('country.skipMock', true);
-
-    mod.run(['country.context', 'MockModule.service', 'country.skipMock', function (context, mockService, skip) {
-            mockService.setMock(context, skip);
-        }]);
+    mod.constant('country.skipMock', false);
 
     mod.constant('countryModel', [{
             name: 'name',
