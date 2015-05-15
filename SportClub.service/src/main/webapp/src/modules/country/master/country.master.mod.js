4e4eb24c46921c9840ac7b31ad1305676abf7197
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-05-15 16:59:04 -0500

    MD Correcciones Mocks MD

--- SportClub.service/src/main/webapp/src/modules/country/master/country.master.mod.js
+++ SportClub.service/src/main/webapp/src/modules/country/master/country.master.mod.js
@@ -2,10 +2,4 @@
     var mod = ng.module('countryMasterModule', ['countryModule', 'sportModule', 'masterModule']);
 
     mod.constant('countryMasterModule.context', 'countries/master');
-    
-    mod.constant('countryMasterModule.skipMock', true);
-
-    mod.run(['countryMasterModule.skipMock', 'countryMasterModule.context', 'MockModule.service', function (skip, ctx, svc) {
-            svc.setMock(ctx, skip);
-        }]);
 })(window.angular);
