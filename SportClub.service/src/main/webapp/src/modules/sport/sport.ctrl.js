7f5914daba935adf6a73a05fd0d4319c7b7a39e1
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-05-20 21:58:35 -0500

    Sport AvgAge Se elimina método no usado

--- SportClub.service/src/main/webapp/src/modules/sport/sport.ctrl.js
+++ SportClub.service/src/main/webapp/src/modules/sport/sport.ctrl.js
@@ -4,9 +4,6 @@
     sportModule.controller('sportCtrl', ['$scope', 'sportService','countryService', 'sportModel', function ($scope, sportSvc, countrySvc, model) {
             sportSvc.extendController(this, $scope, model);
             this.fetchRecords();
-            this.getAvgAge = function (sport) {
-                return (sport.minAge + sport.maxAge) / 2;
-            };
             var getCountryName = function(record){
                 for (var i in $scope.countryRecords) {
                     if ($scope.countryRecords[i].id === record.country) {
