a05175f41beba694874668c18ac22e6b201265f9
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-05-26 16:04:00 -0500

    Miscelaneos - Se cambia nombre de funcion de calculo de campo de compute a fn - Se quita campo de orden en el modelo - Se identa código - Se implementa código html para funcionalidad en IE

--- SportClub.service/src/main/webapp/src/modules/sport/sport.mod.js
+++ SportClub.service/src/main/webapp/src/modules/sport/sport.mod.js
@@ -2,41 +2,35 @@
     var mod = ng.module('sportModule', ['CrudModule', 'countryModule']);
 
     mod.constant('sport.context', 'sports');
-    
+
     mod.constant('sportModel', [{
             name: 'name',
             displayName: 'Name',
-            type: 'String',
-            order: 1
+            type: 'String'
         }, {
             name: 'minAge',
             displayName: 'MinAge',
-            type: 'Integer',
-            order: 2
+            type: 'Integer'
         }, {
             name: 'maxAge',
             displayName: 'MaxAge',
-            type: 'Integer',
-            order: 3
+            type: 'Integer'
         }, {
             name: 'avgAge',
             displayName: 'Average Age',
             type: 'Computed',
-            order: 4,
-            compute: function (record) {
+            fn: function (record) {
                 return (record.minAge + record.maxAge) / 2;
             }
         }, {
             name: 'rules',
             displayName: 'Rules',
-            type: 'String',
-            order: 5
+            type: 'String'
         }, {
             name: 'country',
             displayName: 'Country',
             type: 'Computed',
-            order: 6,
-            compute: function (record) {
+            fn: function (record) {
                 return record.country;
             }
         }]);
--- SportClub.service/src/main/webapp/src/shared/crud/crud.ctrl.js
+++ SportClub.service/src/main/webapp/src/shared/crud/crud.ctrl.js
@@ -1,18 +1,23 @@
 (function (ng) {
     var mod = ng.module('CrudModule');
-    
-    mod.controller('listCtrl', ['$scope', function($scope){
-            $scope.checkAll = function(){
-                this.records.forEach(function(item){item.selected = !item.selected;});
+
+    mod.controller('listCtrl', ['$scope', function ($scope) {
+            $scope.checkAll = function () {
+                this.records.forEach(function (item) {
+                    item.selected = !item.selected;
+                });
             };
-    }]);
+        }]);
 
     mod.controller('modalCtrl', ['$scope', '$modalInstance', 'items', 'name', function ($scope, $modalInstance, items, name) {
+            $scope.model = [{name: 'name', displayName: 'Name', type: 'String', order: 1}];
             $scope.name = name;
             $scope.items = items;
 
             function getSelectedItems() {
-                return $scope.items.filter(function(item){return !!item.selected;});
+                return $scope.items.filter(function (item) {
+                    return !!item.selected;
+                });
             }
 
             $scope.ok = function () {
